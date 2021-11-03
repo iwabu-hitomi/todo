a80df2ddf8a9c527a2d824c482f5e4a4c24b6af6
iwabu-hitomi <hitomiiiin3@gmail.com>
2021-11-03 16:44:04 +0900

    firstcommit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Folder; // ★ 追加
+use App\Policies\FolderPolicy; // ★ 追加
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+
+        Folder::class => FolderPolicy::class,
+        // 'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
